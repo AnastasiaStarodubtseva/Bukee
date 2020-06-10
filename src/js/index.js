07a63e88f8762bc3495d3b7a1f58c8bed7e0fbc3
Anastasia Starodubtseva <anastasiast1605@gmail.com>
2020-06-10 16:43:59 +0700

    Fix the menu for mobile responsive

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,9 @@
 import "./import/modules";
+
+let headerList = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+
+navBarToggle.addEventListener('click', function() {
+  headerList.classList.toggle('active');
+});
+
